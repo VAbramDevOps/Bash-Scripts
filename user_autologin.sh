a1a29a3646fb53739dca53a31b5348cde2833c91
VAbramDevOPs <vabram.devops.acc0@gmail.com>
2022-09-07 09:39:15 +0300

    User autologin in Xubuntu

--- user_autologin.sh
+++ user_autologin.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-cd /etc/lightdm/
-touch lightdm.conf
+function autologinConfig() {
+    touch lightdm.conf
+    echo "[SeatDefaults]" >> /etc/lightdm/lightdm.conf
+    echo "autologin-user=$username" >> /etc/lightdm/lightdm.conf
+    echo "autologin-user-timeout=0" >> /etc/lightdm/lightdm.conf
+    echo "user-session=Xubuntu" >> /etc/lightdm/lightdm.conf
+    echo "greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf
+}
+
+cd /etc/lightdm
 
 read -r -p "Enter username for autologin: " username
 
 grep $username /etc/passwd >/dev/null
 if [ $? -ne 0 ]; then
-echo 'No username found!'
+    echo 'ATTENTION: No username found!'
 else 
-echo "[SeatDefaults]" >> /etc/lightdm/lightdm.conf
-echo "autologin-user=$username" >> /etc/lightdm/lightdm.conf
-echo "autologin-user-timeout=0" >> /etc/lightdm/lightdm.conf
-echo "user-session=Xubuntu" >> /etc/lightdm/lightdm.conf
-echo "greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf
-
-echo "AUTOLOGIN FOR USER: $ursername SUCCESSFULLY SET UP"
+    if [[ ! -f lightdm.conf ]]; then
+        autologinConfig;
+    else
+        echo -n > lightdm.conf
+        autologinConfig;
+    fi
+    echo "AUTOLOGIN FOR USER: $ursername SUCCESSFULLY SET UP"
 fi
 
